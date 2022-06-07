77526d69f40786c228f3922cd8889b1a0f0871a6
bocheng24 <jeffcgp@hotmail.com>
2022-06-07 17:15:42 +0800

    aggregation streaming

---
+++ spark-notebooks/transform-streaming/aggStream.py
@@ -0,0 +1,48 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+HOSTNAME = '192.168.2.35'
+PORT = 9999
+
+spark = SparkSession.builder \
+                    .appName('Streaming Aggregation') \
+                    .master('local[2]') \
+                    .getOrCreate()
+
+def socketLines(host, port):
+
+    lines = spark.readStream \
+                 .format('socket') \
+                 .option('host', host) \
+                 .option('port', port) \
+                 .load()
+    
+    return lines
+
+def countStreamLines(lines):
+
+    counts = lines.selectExpr('count(*) as total')
+
+    counts.writeStream \
+          .format('console') \
+          .outputMode('complete') \
+          .start() \
+          .awaitTermination()
+
+def sumNums(lines):
+
+    price = lines.select(col('value').cast('double').alias('price'))
+
+    total = price.select(sum(col('price')).alias('Total Price'))
+
+    total.writeStream \
+         .format('console') \
+         .outputMode('complete') \
+         .start() \
+         .awaitTermination()
+
+if __name__ == '__main__':
+
+    lines = socketLines(HOSTNAME, PORT)
+    # countStreamLines(lines)
+    sumNums(lines)
