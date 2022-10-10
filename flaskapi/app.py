97906a21b33ce53e13c7a187b9723f5e36bf3e3e
bocheng24 <jeffcgp@hotmail.com>
2022-10-10 21:13:26 +0800

    delete store, item | put item

--- flaskapi/app.py
+++ flaskapi/app.py
@@ -26,6 +26,43 @@ def create_store():
 
     return new_store, 201
 
+@app.get('/store/<string:store_id>')
+def get_store(store_id):
+
+    try:
+        return stores[store_id], 200
+
+    except KeyError:
+        return {'message': 'Store not found'}, 404
+
+@app.delete('/store/<string:store_id>')
+def del_store(store_id):
+    try:
+        store = stores[store_id]
+        del stores[store_id]
+        return {
+            'message': f'{store_id} deleted',
+            'data': store
+        }
+
+    except:
+        return {'message': 'Store not found'}, 404
+
+@app.get('/items')
+def get_all_items():
+
+    return {'items': list(items.values())}
+
+
+
+@app.get('/items/<string:item_id>')
+def get_item_in_store(item_id):
+    try:
+        return items[item_id], 200
+    
+    except KeyError:
+        return {'message': 'Item not found'}, 404
+
 @app.post('/item')
 def add_item():
     item_data = request.get_json()
@@ -45,24 +82,36 @@ def add_item():
 
     return {'message': 'Store not found'}, 404
 
-@app.get('/items')
-def get_all_items():
-
-    return {'items': list(items.values())}
-
-@app.get('/store/<string:store_id>')
-def get_store(store_id):
+@app.delete('/item/<string:item_id>')
+def del_item(item_id):
 
     try:
-        return stores[store_id], 200
-
-    except KeyError:
-        return {'message': 'Store not found'}, 404
-
-@app.get('/items/<string:item_id>')
-def get_item_in_store(item_id):
-    try:
-        return items[item_id], 200
+        item = items[item_id]
+        del items[item_id]
+        return {
+            'message': f'{item_id} deleted',
+            'data': item
+        }
     
     except KeyError:
         return {'message': 'Item not found'}, 404
+
+@app.put('/item/<string:item_id>')
+def update_item(item_id):
+
+    item_up = request.get_json()
+
+    if 'name' not in item_up or 'price' not in item_up:
+        return {'message': "Bad request. Ensure 'price', and 'name' are included in the JSON payload."}, 400
+    
+    try:
+        items[item_id] |= item_up
+
+        return {
+            'message': f'{item_id} updated',
+            'data': items[item_id]
+        }
+    
+    except:
+        return {'message': 'Item not found'}, 404
+        
